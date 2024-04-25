4d36e34990bd0a45f345a5563ebaa9a4e4e3a3c9
fadhli <fadliprestigious@gmail.com>
2024-04-26 04:23:03 +0800

    Initial Commit

---
+++ Recipe API App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe API App
+//
+//  Created by Fadhli Firdaus on 26/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe API App/Recipe_API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_API_AppApp.swift
+//  Recipe API App
+//
+//  Created by Fadhli Firdaus on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
